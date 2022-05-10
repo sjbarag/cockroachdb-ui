004bb54b846e2c48ede4d7f394f13c04723a7c73
Sean Barag <barag@cockroachlabs.com>
2022-05-10 22:56:20 +0000

    icons: import chalk as ES Module

--- packages/icons/notests.js
+++ packages/icons/notests.js
@@ -1,16 +1,18 @@
-const emoji = require("node-emoji");
-const chalk = require("chalk");
+(async function(){
+  const emoji = require("node-emoji");
+  const chalk = (await import("chalk")).default;
 
-console.log(`
-${emoji.emojify(
-  `:ghost: :ghost: :ghost: ${chalk.bold.red("Nope!")} :ghost: :ghost: :ghost:`,
-)}
+  console.log(`
+  ${emoji.emojify(
+    `:ghost: :ghost: :ghost: ${chalk.bold.red("Nope!")} :ghost: :ghost: :ghost:`,
+  )}
 
-There are no tests here because this package contains no code.
-It is fully dependent on ${emoji.get("package")} ${chalk.bold(
-  "@svgr/cli",
-)} ${emoji.get("package")}, so you can look at their
-tests at ${chalk.cyanBright(
-  "https://github.com/gregberge/svgr/tree/master/packages/cli",
-)}.
-`);
+  There are no tests here because this package contains no code.
+  It is fully dependent on ${emoji.get("package")} ${chalk.bold(
+    "@svgr/cli",
+  )} ${emoji.get("package")}, so you can look at their
+  tests at ${chalk.cyanBright(
+    "https://github.com/gregberge/svgr/tree/master/packages/cli",
+  )}.
+  `);
+})();
